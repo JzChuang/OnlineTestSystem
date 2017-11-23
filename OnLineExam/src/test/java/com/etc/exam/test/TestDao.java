b8f2f55e61d1be0e36f783a3b52129caca1aac09
看看不懂 <825941503@qq.com>
2017-11-23 10:24:09 +0800

    注册登录即将完成

--- OnLineExam/src/test/java/com/etc/exam/test/TestDao.java
+++ OnLineExam/src/test/java/com/etc/exam/test/TestDao.java
@@ -8,7 +8,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.etc.exam.dao.ExamTypeMapper;
+import com.etc.exam.dao.UsersMapper;
 import com.etc.exam.entity.ExamType;
+import com.etc.exam.entity.Users;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration(locations= {"",""})
@@ -16,6 +18,8 @@ import com.etc.exam.entity.ExamType;
 public class TestDao {
 	@Resource
 	private ExamTypeMapper etm;
+	@Resource
+	private UsersMapper um;
 	
 	@Test
 	public void testExamTypeMapper() {
@@ -25,6 +29,14 @@ public class TestDao {
 		System.out.println("000");
 	}
 	
-	
+	@Test
+	public void testUsersMapper() {
+//		String USERNAME="jzc";
+//		Users u =um.selectStuByName(USERNAME);
+//		System.out.println(u);
+		Users u = new Users(5, "17720837681", "123456", "Ñ§Éú");
+		int i = um.insert(u);
+		System.out.println(i);
+	}
 	
 }
